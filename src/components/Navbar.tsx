cf739e6deac3534fc080355cb4d702aeac411c81
Elliot Bobrow <elliotgreybobrow@gmail.com>
2021-02-16 13:49:57 -0800

    Only render protected routes when logged in to prevent attempted notification fetching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { UserInfo } from './UserInfo';
 import { FlexContainer, NavContainer } from './styled/Auth';
 import { ProtectedRoutes } from './ProtectedRoutes';
+import { useUser } from '../hooks/useUser';
 
 export const Navbar: React.FC = () => {
+  const { data: user, isLoading } = useUser();
+
   return (
     <AppBar position="relative" color="primary">
       <NavContainer>
         <FlexContainer width="100%">
-          <ProtectedRoutes />
+          {user && !isLoading ? <ProtectedRoutes /> : <div></div>}
 
           <UserInfo />
         </FlexContainer>
--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,7 +1,6 @@
 import { Badge, Button, ButtonGroup } from '@material-ui/core';
 import React from 'react';
 import { useNotifications } from '../hooks/useNotifications';
-import { useUser } from '../hooks/useUser';
 import { StyledLink } from './styled/Auth';
 
 const PROTECTED_ROUTES = [
@@ -16,7 +15,6 @@ const PROTECTED_ROUTES = [
 ];
 
 export const ProtectedRoutes: React.FC = () => {
-  const { data: user, isLoading } = useUser();
   const { data } = useNotifications();
 
   return (
@@ -26,19 +24,17 @@ export const ProtectedRoutes: React.FC = () => {
           <StyledLink to="/">Home</StyledLink>
         </Button>
       </Badge>
-      {user && !isLoading
-        ? PROTECTED_ROUTES.map(route => (
-            <Badge
-              key={route.url}
-              color="secondary"
-              variant="dot"
-              invisible={!data || !data[route.name]?.new}>
-              <Button>
-                <StyledLink to={route.url}>{route.name}</StyledLink>
-              </Button>
-            </Badge>
-          ))
-        : ''}
+      {PROTECTED_ROUTES.map(route => (
+        <Badge
+          key={route.url}
+          color="secondary"
+          variant="dot"
+          invisible={!data || !data[route.name]?.new}>
+          <Button>
+            <StyledLink to={route.url}>{route.name}</StyledLink>
+          </Button>
+        </Badge>
+      ))}
     </ButtonGroup>
   );
 };
